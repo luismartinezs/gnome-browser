b040e1786ba1d0515e73ca629035773024767f73
Luis Martinez <martinez.s.luis@gmail.com>
2018-11-07 16:19:28 +0100

    Updates

---
+++ precache-manifest.4eb275329b560e0f0bcea3d41f572d69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcef6e29a41bcec07bc1",
+    "url": "/gnome-browser/static/js/runtime~main.dcef6e29.js"
+  },
+  {
+    "revision": "33aaac6d2f2872e5022f",
+    "url": "/gnome-browser/static/js/main.33aaac6d.chunk.js"
+  },
+  {
+    "revision": "664f9acfeef08c305168",
+    "url": "/gnome-browser/static/js/1.664f9acf.chunk.js"
+  },
+  {
+    "revision": "33aaac6d2f2872e5022f",
+    "url": "/gnome-browser/static/css/main.012fb623.chunk.css"
+  },
+  {
+    "revision": "41dd1df9f43a4603172d9978bd19adfd",
+    "url": "/gnome-browser/index.html"
+  }
+];
